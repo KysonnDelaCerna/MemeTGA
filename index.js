2dd4df08bab1a4803a0c1594f7a3661fd651fff4
KysonnDelaCerna <Kysonndelacerna@gmail.com>
2020-03-03 16:38:21 +0800

    started website

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    let colors = ["W", "U", "B", "R", "G"];
+    let sets = ["thb", "eld", "m20", "war", "rna", "grn", "m19", "dar", "rix", "xln"];
+    let format = "historic";
+    let rarity = ["common", "uncommon", "rare", "mythic"];
+
+    let deckSize = 60;
+    let averageCMC = 4.0;
+});
