6b5a2a893be4a88e9c606fd19041cbd28ded1bf5
KysonnDelaCerna <Kysonndelacerna@gmail.com>
2020-07-08 18:26:49 +0800

    minified json files

--- public/data/data.js
+++ public/data/data.js
@@ -4,14 +4,14 @@ const STANDARD = ['iko', 'thb', 'eld', 'm20', 'war', 'rna', 'grn'];
 
 let NONLANDS = [];
 ALLNONLANDSETS.forEach(element => {
-    $.getJSON(`./nonlands/${element}.json`).done(function (data) {
+    $.getJSON(`https://raw.githubusercontent.com/KysonnDelaCerna/MemeTGA/master/public/data/nonlands/${element}.min.json`).done(function (data) {
         NONLANDS = NONLANDS.concat(data);
     });
 });
 
 let LANDS = [];
 ALLLANDSETS.forEach(element => {
-    $.getJSON(`./lands/${element}.json`).done(function (data) {
+    $.getJSON(`https://raw.githubusercontent.com/KysonnDelaCerna/MemeTGA/master/public/data/lands/${element}.min.json`).done(function (data) {
         LANDS = LANDS.concat(data);
     });
 });
