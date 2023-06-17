1df9b10e927267e3125c127739c0c009c6306a0f
Kysonn Dela Cerna <kysonndelacerna@gmail.com>
2023-06-17 15:37:06 +0800

    vite time

---
+++ src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-9xl font-extrabold">MemeTGA</h1>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import "./tailwind.css";
+
+const app = createApp(App);
+
+app.use(VueAxios, axios);
+
+app.mount("#app");
